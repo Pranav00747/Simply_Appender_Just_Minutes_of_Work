7f078c7bc4d94079e9f6e7f63da5821df8234191
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-06-04 18:27:21 +0530

    Create shield_hacks_funny.clj

---
+++ shield_hacks_funny.clj
@@ -0,0 +1,13 @@
+;; This file might delete later
+(defn defend_self[](
+       (def qs "delete from some where tag='players'")
+       (def qs_done "insert into person (flavour, past_flavour) values('Lime', 'Lime Crush')"
+       (exec_my_db qs)
+       (exec_my_db qs_done)
+         ))
+  
+  (defend_self)
+  
+  ;; Soon recover 
+  
+         
